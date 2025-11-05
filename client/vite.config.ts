f98e8beabd00441903b32d8d9af2b15e20a6ff1a
Stoyan Germanov <s.p.germanov@gmail.com>
2025-11-05 11:37:48 +0200

    fix PWA commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         react(),
         VitePWA({
-            registerType: 'autoUpdate',
+            registerType: "autoUpdate",
             manifest: {
-                name: 'My PWA App',
-                short_name: 'MyPWA',
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#ffffff',
-                theme_color: '#007acc',
+                name: "January Calc",
+                short_name: "Calc",
+                start_url: "/january-calc/",
+                scope: "/january-calc/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#007acc",
                 icons: [
-                    { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
-                    { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' },
+                    {
+                        src: "/january-calc/pwa-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/january-calc/pwa-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
                 ],
             },
         }),
     ],
+    base: "/january-calc/",
 });
