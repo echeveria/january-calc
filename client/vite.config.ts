ca78d220329fc71755bb0f14224b2d96407bc784
Stoyan Germanov <s.p.germanov@gmail.com>
2025-11-05 11:51:18 +0200

    link fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+    base: "/january-calc/",
     plugins: [
         react(),
         VitePWA({
@@ -30,5 +31,4 @@ export default defineConfig({
             },
         }),
     ],
-    base: "/january-calc/",
 });
