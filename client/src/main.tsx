b57051ff19100236c713081afd19a99914776ebf
Stoyan Germanov <s.p.germanov@gmail.com>
2025-11-05 12:14:31 +0200

    style fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { registerSW } from './registerServiceWorker';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+import "./index.css";
+import { registerSW } from "./registerServiceWorker";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
 
 registerSW();
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     base: "/january-calc/",
     plugins: [
+        tailwindcss(),
         react(),
         VitePWA({
             registerType: "autoUpdate",
